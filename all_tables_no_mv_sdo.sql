1bd502570ae9e05af664a982518131d1cc4924f3
tounou <tounou@email.arizona.edu>
2017-01-11 15:52:55 -0700

    Update all_tables_no_mv_sdo.sql

--- all_tables_no_mv_sdo.sql
+++ all_tables_no_mv_sdo.sql
@@ -3,4 +3,4 @@
 
 -- Return list of all tables
 
-select table_name from all_tables where table_name not like '%$%'and table_name not like '%SDO%';
+select table_name from all_tables where table_name not like '%$%'and table_name not like '%SDO%' order by table_name;
