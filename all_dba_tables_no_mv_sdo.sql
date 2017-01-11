7da46c2c8d4722327aa2f6ee5c506caeacf08a06
tounou <tounou@email.arizona.edu>
2017-01-11 15:10:40 -0700

    Create all_dba_tables_no_mv_sdo.sql

---
+++ all_dba_tables_no_mv_sdo.sql
@@ -0,0 +1,6 @@
+-- Returns the number of records in DBA
+ select count(table_name) from dba_tables where table_name not like '%$%'and table_name not like '%SDO%';
+
+-- Return list of all tables
+
+select table_name from dba_tables where table_name not like '%$%'and table_name not like '%SDO%';
