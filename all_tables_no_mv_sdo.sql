e452137ec6d4d15d74aa97b1bddc4361b3ac3c52
tounou <tounou@email.arizona.edu>
2017-01-12 10:10:48 -0700

    Update all_tables_no_mv_sdo.sql

--- all_tables_no_mv_sdo.sql
+++ all_tables_no_mv_sdo.sql
@@ -1,6 +1,6 @@
 -- Returns the number of records in DBA
- select count(table_name) from all_tables where table_name not like '%$%'and table_name not like '%SDO%';
+ select count(table_name) from all_tables where table_name not like '%$%'and table_name not like 'SDO%';
 
 -- Return list of all tables
 
-select table_name from all_tables where table_name not like '%$%'and table_name not like '%SDO%' order by table_name;
+select table_name from all_tables where table_name not like '%$%'and table_name not like 'SDO%' order by table_name;
