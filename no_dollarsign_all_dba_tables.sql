ea96bbf3f0d429eae419aa4fb5796657b497da8c
tounou <tounou@email.arizona.edu>
2017-01-12 12:21:47 -0700

    Update no_dollarsign_all_dba_tables.sql

--- no_dollarsign_all_dba_tables.sql
+++ no_dollarsign_all_dba_tables.sql
@@ -1,6 +1,6 @@
 -- Returns the number of records in DBA 
-2  select count(table_name) from dba_tables where table_name not like '%$%'; 
-3 
-4 -- Return list of all tables 
-5  
-6 select table_name from dba_tables where table_name not like '%$%' order by table_name; 
+select count(table_name) from dba_tables where table_name not like '%$%'; 
+ 
+-- Return list of all tables 
+  
+select table_name from dba_tables where table_name not like '%$%' order by table_name; 
