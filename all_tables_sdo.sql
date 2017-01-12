2ca415ca99fa503233906c71900e777a15102f64
tounou <tounou@email.arizona.edu>
2017-01-12 09:52:15 -0700

    Update all_tables_sdo.sql

--- all_tables_sdo.sql
+++ all_tables_sdo.sql
@@ -1,6 +1,6 @@
 -- Returns the number of records in All_tables
- select count(table_name) from all_tables where table_name like '%sdo%';
+ select count(table_name) from all_tables where table_name like '%SDO%';
 
 -- Return list of all tables
 
-select table_name from all_tables where table_name like '%sdo%' order by table_name;
+select table_name from all_tables where table_name like '%SDO%' order by table_name;
