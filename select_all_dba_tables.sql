ea16ad8f27b075453a86e845c6a1a2f8a0acca67
tounou <tounou@email.arizona.edu>
2017-01-12 11:51:30 -0700

    Delete select_all_dba_tables.sql

--- select_all_dba_tables.sql
+++
@@ -1,9 +0,0 @@
-
--- Returns the number of records in DBA
- select count(table_name) from dba_tables where table_name not like '%$%';
-
--- Return list of all tables
-
-select table_name from dba_tables where table_name not like '%$%' order by table_name;
-
-
