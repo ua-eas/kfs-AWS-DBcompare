13fc62163735b8258058b762098e5e4f42dec780
tounou <tounou@email.arizona.edu>
2017-02-14 13:59:35 -0700

    Delete all_tables_no_mv_sdo.sql

--- all_tables_no_mv_sdo.sql
+++
@@ -1,6 +0,0 @@
--- Returns the number of records in DBA
- select count(table_name) from all_tables where table_name not like '%$%'and table_name not like 'SDO%';
-
--- Return list of all tables
-
-select table_name from all_tables where table_name not like '%$%'and table_name not like 'SDO%' order by table_name;
