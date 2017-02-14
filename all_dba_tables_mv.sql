92c612ae0f86d4429a33f915a0955286ebcac947
tounou <tounou@email.arizona.edu>
2017-02-14 13:59:06 -0700

    Delete all_dba_tables_mv.sql

--- all_dba_tables_mv.sql
+++
@@ -1,6 +0,0 @@
--- Returns the number of records in DBA
- select count(table_name) from dba_tables where table_name like '%$%';
-
--- Return list of all tables
-
-select table_name from dba_tables where table_name like '%$%' order by table_name;
