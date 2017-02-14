3658550a1fba28d2d3f07edf24e491e28670151b
tounou <tounou@email.arizona.edu>
2017-02-14 13:59:24 -0700

    Delete all_dba_tables_sdo.sql

--- all_dba_tables_sdo.sql
+++
@@ -1,6 +0,0 @@
--- Returns the number of records in All_tables
- select count(table_name) from dba_tables where table_name like 'SDO%';
-
--- Return list of all tables
-
-select table_name from dba_tables where table_name like 'SDO%' order by table_name;
