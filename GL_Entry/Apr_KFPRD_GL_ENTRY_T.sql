1b2850ea2c6873afc3ccf4d8e4675248001c94e2
tounou <tounou@email.arizona.edu>
2017-02-17 08:53:31 -0700

    Update Apr_KFPRD_GL_ENTRY_T.sql

--- GL_Entry/Apr_KFPRD_GL_ENTRY_T.sql
+++ GL_Entry/Apr_KFPRD_GL_ENTRY_T.sql
@@ -28,7 +28,7 @@ FDOC_REF_TYP_CD,
 FS_REF_ORIGIN_CD 
 from kulowner.GL_ENTRY_T@uazkfprd 
 WHERE TRANSACTION_DT BETWEEN TO_DATE ('YEAR/04/01', 'yyyy/mm/dd')
-AND TO_DATE ('YEAR/04/01', 'yyyy/mm/dd')
+AND TO_DATE ('YEAR/04/30', 'yyyy/mm/dd')
 order by UNIV_FISCAL_YR,
 TRN_LDGR_ENTR_AMT,
 FIN_BALANCE_TYP_CD,
