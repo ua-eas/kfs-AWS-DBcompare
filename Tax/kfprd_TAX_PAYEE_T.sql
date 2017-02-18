a1b5bd1f5dc76e6055a79f032e412aaff1e636bf
tounou <tounou@email.arizona.edu>
2017-02-18 09:04:17 -0700

    Update kfprd_TAX_PAYEE_T.sql

--- Tax/kfprd_TAX_PAYEE_T.sql
+++ Tax/kfprd_TAX_PAYEE_T.sql
@@ -1,6 +1,6 @@
-SELECT CURRENT_TIMESTAMP FROM DUAL;
+;SELECT CURRENT_TIMESTAMP FROM DUAL;
 
 set markup html on spool on 
 spool ./data/kfprd.xls
-select count(1) from KULOWNER.tax_payee_t@uazkfprd
+select count(1) from KULOWNER.tax_payee_t@uazkfprd;
 select * from KULOWNER.tax_payee_t@uazkfprd  order by payee_id , VNDR_HDR_GNRTD_ID asc;
