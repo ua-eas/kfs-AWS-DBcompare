7b8284b7ed4c97eedfcd0f641dd07a86817d3402
tounou <tounou@email.arizona.edu>
2017-02-18 08:30:41 -0700

    Update kfimp_TAX_PAYEE_T.sql

--- Tax/kfimp_TAX_PAYEE_T.sql
+++ Tax/kfimp_TAX_PAYEE_T.sql
@@ -1,5 +1,8 @@
+
 SELECT CURRENT_TIMESTAMP FROM DUAL;
 
 set markup html on spool on 
 spool ./data/kfimp.xls
+
+select count(1) from kulowner.GL_ENTRY_T;
 select * from KULOWNER.tax_payee_t order by payee_id , VNDR_HDR_GNRTD_ID asc;
