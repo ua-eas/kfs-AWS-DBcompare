21cd862e1872102701f9266be84ec3ef3c48b6c0
tounou <tounou@email.arizona.edu>
2017-02-20 16:45:59 -0700

    Update KFIMP_tax_payment_t.sql

--- Tax/KFIMP_tax_payment_t.sql
+++ Tax/KFIMP_tax_payment_t.sql
@@ -1,7 +1,22 @@
-select * from kulowner.tax_payment_t
+SELECT CURRENT_TIMESTAMP FROM DUAL;
+
+set markup html on spool on 
+
+spool ./data/kfimp.xls
+
+select PAYMENT_ID,
+PAYMENT_GRP_ID,
+PMT_ACCT_DETAIL_ID,
+ACCOUNT_NBR,
+PAYEE_ID,
+DISB_NBR  from kulowner.tax_payment_t
 order by PAYMENT_ID,
 PAYMENT_GRP_ID,
 PMT_ACCT_DETAIL_ID,
 ACCOUNT_NBR,
 PAYEE_ID,
 DISB_NBR  ASC;
+
+
+spool off
+
