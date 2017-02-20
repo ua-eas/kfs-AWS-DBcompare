03dd357ea57e58a97953cb61d826aaa2d26ead5d
tounou <tounou@email.arizona.edu>
2017-02-20 16:30:01 -0700

    Create kfprd_tax_payment_t.sql

---
+++ Tax/kfprd_tax_payment_t.sql
@@ -0,0 +1,22 @@
+SELECT CURRENT_TIMESTAMP FROM DUAL;
+
+set markup html on spool on 
+
+spool ./data/kfimp.xls
+
+select PAYMENT_ID,
+PAYMENT_GRP_ID,
+PMT_ACCT_DETAIL_ID,
+ACCOUNT_NBR,
+PAYEE_ID,
+DISB_NBR  from kulowner.tax_payment_t@uazkfprd
+order by PAYMENT_ID,
+PAYMENT_GRP_ID,
+PMT_ACCT_DETAIL_ID,
+ACCOUNT_NBR,
+PAYEE_ID,
+DISB_NBR  ASC;
+
+
+spool off
+
