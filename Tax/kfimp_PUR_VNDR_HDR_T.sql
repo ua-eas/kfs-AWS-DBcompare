489718fab9a7490a4fc2964a25204c24aef03543
tounou <tounou@email.arizona.edu>
2017-02-20 20:57:47 -0700

    Create kfimp_PUR_VNDR_HDR_T.sql

---
+++ Tax/kfimp_PUR_VNDR_HDR_T.sql
@@ -0,0 +1,11 @@
+SELECT CURRENT_TIMESTAMP FROM DUAL;
+
+set markup html on spool on 
+
+spool ./data/kfimp.xls
+
+select * from kulowner.PUR_VNDR_HDR_T
+order by VNDR_HDR_GNRTD_ID, OBJ_ID, VER_NBR ASC;
+
+
+spool off
