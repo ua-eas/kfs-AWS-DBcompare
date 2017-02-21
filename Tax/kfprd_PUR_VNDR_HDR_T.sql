692252719c23c44f3fc0a7e7d4d36e33d2b7c28d
tounou <tounou@email.arizona.edu>
2017-02-20 21:00:16 -0700

    Update kfprd_PUR_VNDR_HDR_T.sql

--- Tax/kfprd_PUR_VNDR_HDR_T.sql
+++ Tax/kfprd_PUR_VNDR_HDR_T.sql
@@ -1,1 +1,13 @@
-PUR_VNDR_HDR_T
+SELECT CURRENT_TIMESTAMP FROM DUAL;
+
+set markup html on spool on 
+
+spool ./data/kfprd.xls
+
+select * from kulowner.PUR_VNDR_HDR_T@uazkfprd
+order by VNDR_HDR_GNRTD_ID, OBJ_ID, VER_NBR ASC;
+
+
+spool off
+
+
