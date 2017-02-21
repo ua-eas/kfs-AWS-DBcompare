16d5632af53360c8c4d1c36aa2843cb614a633e6
tounou <tounou@email.arizona.edu>
2017-02-21 10:55:09 -0700

    Update kfprd_FP_PRCRMNT_TRN_DTL_T.sql

--- Tax/kfprd_FP_PRCRMNT_TRN_DTL_T.sql
+++ Tax/kfprd_FP_PRCRMNT_TRN_DTL_T.sql
@@ -2,7 +2,7 @@ SELECT CURRENT_TIMESTAMP FROM DUAL;
 
 set markup html on spool on 
 
-spool ./data/kfimp.xls
+spool ./data/kfprd.xls
 
 select * from kulowner.FP_PRCRMNT_TRN_DTL_T@uazkfprd
 order by FDOC_NBR,OBJ_ID,TRN_REF_NBR asc;
