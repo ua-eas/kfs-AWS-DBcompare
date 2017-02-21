4a5d905d8a8d3f429db2d766c4f6b48241ca947b
tounou <tounou@email.arizona.edu>
2017-02-20 21:35:53 -0700

    Create kfimp_FP_PRCRMNT_LVL3_ADD_T.sql

---
+++ Tax/kfimp_FP_PRCRMNT_LVL3_ADD_T.sql
@@ -0,0 +1,12 @@
+SELECT CURRENT_TIMESTAMP FROM DUAL;
+
+set markup html on spool on 
+
+spool ./data/kfimp.xls
+
+select * from kulowner.FP_PRCRMNT_LVL3_ADD_T
+order by FDOC_NBR, OBJ_ID asc;
+
+
+
+spool off
