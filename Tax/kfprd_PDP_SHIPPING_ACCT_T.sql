28fd7ad6ab4bf1079594767c139e4d9daa4ab944
tounou <tounou@email.arizona.edu>
2017-02-20 21:21:53 -0700

    Update kfprd_PDP_SHIPPING_ACCT_T.sql

--- Tax/kfprd_PDP_SHIPPING_ACCT_T.sql
+++ Tax/kfprd_PDP_SHIPPING_ACCT_T.sql
@@ -1,1 +1,12 @@
-PDP_SHIPPING_ACCT_T
+SELECT CURRENT_TIMESTAMP FROM DUAL;
+
+set markup html on spool on 
+
+spool ./data/kfprd.xls
+
+select * from kulowner.PDP_SHIPPING_ACCT_T
+order by SHIPPING_ACCOUNT_NBR, ACCOUNT_NBR, PRNCPL_ID asc;
+
+
+
+spool off
