7439b2b91f1f271ba0086cf1d21b0c4a3dab55df
tounou <tounou@email.arizona.edu>
2017-02-21 12:54:57 -0700

    Create kfimp_AP_CRDT_MEMO_STAT_T.sql

---
+++ Tax/kfimp_AP_CRDT_MEMO_STAT_T.sql
@@ -0,0 +1,12 @@
+SELECT CURRENT_TIMESTAMP FROM DUAL;
+
+set markup html on spool on 
+
+spool ./data/kfimp.xls
+
+select * from kulowner.AP_CRDT_MEMO_STAT_T
+order by CRDT_MEMO_STAT_CD, OBJ_ID asc;
+
+
+
+spool off
