bdbdd2e95449d73088f58ae240bbb0d5fd0bb882
tounou <tounou@email.arizona.edu>
2017-02-20 21:28:38 -0700

    Update kfimp_PUR_VNDR_HDR_T.sql

--- Tax/kfimp_PUR_VNDR_HDR_T.sql
+++ Tax/kfimp_PUR_VNDR_HDR_T.sql
@@ -5,7 +5,7 @@ set markup html on spool on
 spool ./data/kfimp.xls
 
 select * from kulowner.PUR_VNDR_HDR_T
-order by VNDR_HDR_GNRTD_ID, OBJ_ID, VER_NBR ASC;
+order by VNDR_TAX_NBR, VNDR_HDR_GNRTD_ID, OBJ_ID, VER_NBR ASC;
 
 
 spool off
