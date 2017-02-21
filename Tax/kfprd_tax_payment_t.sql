d238458e4b6f91505187fd73a50108740a303e61
tounou <tounou@email.arizona.edu>
2017-02-20 17:13:24 -0700

    Update kfprd_tax_payment_t.sql

--- Tax/kfprd_tax_payment_t.sql
+++ Tax/kfprd_tax_payment_t.sql
@@ -4,12 +4,7 @@ set markup html on spool on
 
 spool ./data/kfprd.xls
 
-select PAYMENT_ID,
-PAYMENT_GRP_ID,
-PMT_ACCT_DETAIL_ID,
-ACCOUNT_NBR,
-PAYEE_ID,
-DISB_NBR  from kulowner.tax_payment_t@uazkfprd
+select * from kulowner.tax_payment_t@uazkfprd
 order by PAYMENT_ID,
 PAYMENT_GRP_ID,
 PMT_ACCT_DETAIL_ID,
