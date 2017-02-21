d53dbe04b8b9d15716a7c0f3c67c392ff03d55c5
tounou <tounou@email.arizona.edu>
2017-02-20 21:39:01 -0700

    Update kfprd_FP_PRCRMNT_LVL3_ADD_T.sql

--- Tax/kfprd_FP_PRCRMNT_LVL3_ADD_T.sql
+++ Tax/kfprd_FP_PRCRMNT_LVL3_ADD_T.sql
@@ -4,7 +4,7 @@ set markup html on spool on
 
 spool ./data/kfprd.xls
 
-select * from kulowner.FP_PRCRMNT_LVL3_ADD_T
+select * from kulowner.FP_PRCRMNT_LVL3_ADD_T@uazkfprd
 order by FDOC_NBR, OBJ_ID asc;
 
 
