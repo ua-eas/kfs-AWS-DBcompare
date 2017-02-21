44549c7546fea14c3f7412ca43910f9c710303ba
tounou <tounou@email.arizona.edu>
2017-02-21 12:47:32 -0700

    Update kfimp_FP_PRCRMNT_LVL3_ADD_T.sql

--- Tax/kfimp_FP_PRCRMNT_LVL3_ADD_T.sql
+++ Tax/kfimp_FP_PRCRMNT_LVL3_ADD_T.sql
@@ -5,7 +5,7 @@ set markup html on spool on
 spool ./data/kfimp.xls
 
 select * from kulowner.FP_PRCRMNT_LVL3_ADD_T
-order by FDOC_NBR, OBJ_ID asc;
+order by FDOC_NBR, OBJ_ID, VER_NBR,DEST_POSTAL asc;
 
 
 
