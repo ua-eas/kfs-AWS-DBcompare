30854e0304778160d10fb905b9d15e567a42999a
tounou <tounou@email.arizona.edu>
2017-02-20 21:54:57 -0700

    Create kfimp_FP_PRCRMNT_TRN_DTL_T.sql

---
+++ Tax/kfimp_FP_PRCRMNT_TRN_DTL_T.sql
@@ -0,0 +1,12 @@
+SELECT CURRENT_TIMESTAMP FROM DUAL;
+
+set markup html on spool on 
+
+spool ./data/kfimp.xls
+
+select * from kulowner.FP_PRCRMNT_TRN_DTL_T
+order by FDOC_NBR,OBJ_ID,TRN_REF_NBR asc;
+
+
+
+spool off
