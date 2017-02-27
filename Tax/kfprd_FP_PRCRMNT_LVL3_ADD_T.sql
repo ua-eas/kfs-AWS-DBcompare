134cc7fc45e095d17ad582f3aecebe97ead9677e
tounou <tounou@email.arizona.edu>
2017-02-27 12:32:24 -0700

    Update kfprd_FP_PRCRMNT_LVL3_ADD_T.sql

--- Tax/kfprd_FP_PRCRMNT_LVL3_ADD_T.sql
+++ Tax/kfprd_FP_PRCRMNT_LVL3_ADD_T.sql
@@ -1,3 +1,4 @@
+
 SELECT CURRENT_TIMESTAMP FROM DUAL;
 
 set markup html on spool on 
@@ -5,6 +6,8 @@ set markup html on spool on
 spool ./data/kfprd.xls
 
 select * from kulowner.FP_PRCRMNT_LVL3_ADD_T@uazkfprd
+WHERE TRANSACTION_DT BETWEEN TO_DATE ('2017/01/01', 'yyyy/mm/dd')
+AND TO_DATE ('2017/03/01', 'yyyy/mm/dd')
 order by FDOC_NBR, OBJ_ID,VER_NBR,DEST_POSTAL  asc;
 
 
