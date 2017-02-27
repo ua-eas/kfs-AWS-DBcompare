640ff011f14d071720e261028cefb8b93cc01fac
tounou <tounou@email.arizona.edu>
2017-02-27 12:31:44 -0700

    Update kfimp_FP_PRCRMNT_LVL3_ADD_T.sql

--- Tax/kfimp_FP_PRCRMNT_LVL3_ADD_T.sql
+++ Tax/kfimp_FP_PRCRMNT_LVL3_ADD_T.sql
@@ -5,6 +5,8 @@ set markup html on spool on
 spool ./data/kfimp.xls
 
 select * from kulowner.FP_PRCRMNT_LVL3_ADD_T
+WHERE TRANSACTION_DT BETWEEN TO_DATE ('2017/01/01', 'yyyy/mm/dd')
+AND TO_DATE ('2017/03/01', 'yyyy/mm/dd')
 order by FDOC_NBR, OBJ_ID, VER_NBR,DEST_POSTAL asc;
 
 
