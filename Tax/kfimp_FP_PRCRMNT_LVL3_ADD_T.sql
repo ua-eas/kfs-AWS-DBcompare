7885a26e532e77e59a4c66ccbefff5227d242ea3
tounou <tounou@email.arizona.edu>
2017-03-01 16:36:39 -0700

    Update kfimp_FP_PRCRMNT_LVL3_ADD_T.sql

--- Tax/kfimp_FP_PRCRMNT_LVL3_ADD_T.sql
+++ Tax/kfimp_FP_PRCRMNT_LVL3_ADD_T.sql
@@ -6,7 +6,7 @@ spool ./data/kfimp1.xls
 
 select * from kulowner.FP_PRCRMNT_LVL3_ADD_T
 WHERE ORDER_DT BETWEEN TO_DATE ('2001/01/01', 'yyyy/mm/dd')
-AND TO_DATE ('2014/12/31', 'yyyy/mm/dd')
+AND TO_DATE ('2013/12/31', 'yyyy/mm/dd')
 order by FDOC_NBR, OBJ_ID, VER_NBR,DEST_POSTAL asc;
 
 
