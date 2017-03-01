d0c4e265e72b38b5c0a32febcea633683b20a17f
tounou <tounou@email.arizona.edu>
2017-03-01 16:11:00 -0700

    Update kfimp_FP_PRCRMNT_TRN_DTL_T.sql

--- Tax/kfimp_FP_PRCRMNT_TRN_DTL_T.sql
+++ Tax/kfimp_FP_PRCRMNT_TRN_DTL_T.sql
@@ -7,7 +7,7 @@ spool ./data/kfimp.xls
 
 select * from kulowner.FP_PRCRMNT_TRN_DTL_T
 WHERE TRANSACTION_DT BETWEEN TO_DATE ('YEAR/01/01', 'yyyy/mm/dd')
-AND TO_DATE ('YEAR/12/31', 'yyyy/mm/dd')
+AND TO_DATE ('YEAR/01/31', 'yyyy/mm/dd')
 order by FDOC_NBR,OBJ_ID,VER_NBR,TRN_REF_NBR,TRN_ORIG_CRNCY_AMT asc;
 
 
