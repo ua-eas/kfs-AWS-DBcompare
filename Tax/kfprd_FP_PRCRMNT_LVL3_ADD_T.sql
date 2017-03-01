66f0e691aaec9e807b4bdde72b32fcf2064e5990
tounou <tounou@email.arizona.edu>
2017-03-01 09:53:32 -0700

    Update kfprd_FP_PRCRMNT_LVL3_ADD_T.sql

--- Tax/kfprd_FP_PRCRMNT_LVL3_ADD_T.sql
+++ Tax/kfprd_FP_PRCRMNT_LVL3_ADD_T.sql
@@ -5,8 +5,8 @@ set markup html on spool on
 spool ./data/kfprd.xls
 
 select * from kulowner.FP_PRCRMNT_LVL3_ADD_T@uazkfprd
-WHERE ORDER_DT  BETWEEN TO_DATE ('2017/01/01', 'yyyy/mm/dd')
-AND TO_DATE ('2017/02/27', 'yyyy/mm/dd')
+WHERE ORDER_DT  BETWEEN TO_DATE ('2016/01/01', 'yyyy/mm/dd')
+AND TO_DATE ('2016/12/31', 'yyyy/mm/dd')
 order by FDOC_NBR, OBJ_ID,VER_NBR,DEST_POSTAL  asc;
 
 
