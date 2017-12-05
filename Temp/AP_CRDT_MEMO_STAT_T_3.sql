ad6ab9bbfcba3645a28617cfed063da696ed451a
mhirstua <32651560+mhirstua@users.noreply.github.com>
2017-12-05 13:45:22 -0700

    Update AP_CRDT_MEMO_STAT_T_3.sql

--- Temp/AP_CRDT_MEMO_STAT_T_3.sql
+++ Temp/AP_CRDT_MEMO_STAT_T_3.sql
@@ -2,7 +2,7 @@ SELECT CURRENT_TIMESTAMP FROM DUAL;
 
 set markup html on spool on 
 
-spool ./data/kf3.xls
+spool ./data/kfs3.xls
 
 select * from kulowner.AP_CRDT_MEMO_STAT_T
 order by CRDT_MEMO_STAT_CD, OBJ_ID asc;
