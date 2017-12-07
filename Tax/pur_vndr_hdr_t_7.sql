fefd88be0597de399fafdadcc13a52c92ac86b1a
Michael Hirst <mhirst@email.arizona.edu>
2017-12-07 15:28:38 -0700

    added pur_vndr sql

---
+++ Tax/pur_vndr_hdr_t_7.sql
@@ -0,0 +1,13 @@
+SELECT CURRENT_TIMESTAMP FROM DUAL;
+
+set markup html on spool on 
+
+spool ./data/pur_vndr_7.xls
+
+select * from kulowner.PUR_VNDR_HDR_T
+order by VNDR_TAX_NBR, VNDR_HDR_GNRTD_ID, OBJ_ID, VER_NBR ASC;
+
+
+spool off
+
+
