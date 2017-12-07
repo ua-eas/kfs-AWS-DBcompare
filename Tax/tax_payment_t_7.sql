14780c6ab3d91d7e5037df585f5f759ead5d5f7a
Michael Hirst <mhirst@email.arizona.edu>
2017-12-07 15:16:21 -0700

    added tax_payment_t sql

---
+++ Tax/tax_payment_t_7.sql
@@ -0,0 +1,17 @@
+SELECT CURRENT_TIMESTAMP FROM DUAL;
+
+set markup html on spool on 
+
+spool ./data/tax_payment_t_gold.xls
+
+select * from kulowner.tax_payment_t
+order by PAYMENT_ID,
+PAYMENT_GRP_ID,
+PMT_ACCT_DETAIL_ID,
+ACCOUNT_NBR,
+PAYEE_ID,
+DISB_NBR  ASC;
+
+
+spool off
+
