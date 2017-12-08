da05f765b74d7b49733a5b2609df80f945d6b757
Michael Hirst <mhirst@email.arizona.edu>
2017-12-08 09:20:51 -0700

    updated ap crdt memo sql

--- Temp/AP_CRDT_MEMO_STAT_T_3.sql
+++ Temp/AP_CRDT_MEMO_STAT_T_3.sql
@@ -2,7 +2,7 @@ SELECT CURRENT_TIMESTAMP FROM DUAL;
 
 set markup html on spool on 
 
-spool ./data/kfs3.xls
+spool ./data/AP_CRDT_MEMO_STAT_T.xls
 
 select * from kulowner.AP_CRDT_MEMO_STAT_T
 order by CRDT_MEMO_STAT_CD, OBJ_ID asc;
