dbefa5c2f0c055eae1e218b0ee4c2bd32ee80769
Michael Hirst <mhirst@email.arizona.edu>
2017-12-12 12:36:41 -0700

    remove space before from line

--- LDGR/KFS7_LD_LDGR_ENTR_T.sql
+++ LDGR/KFS7_LD_LDGR_ENTR_T.sql
@@ -9,7 +9,6 @@ spool ./data/kfs7_ld_ldgr.xls
 
 
 select ll.UNIV_FISCAL_YR, ll.ACCOUNT_NBR, ca.ACCOUNT_NM, cs.FUND_GRP_CD, cs.SUB_FUND_GRP_CD, ll.TRN_LDGR_ENTR_DESC, ll.POSITION_NBR, ll.EMPLID, ca.ORG_CD, co.ORG_NM, co.RPTS_TO_ORG_CD , SUM(CASE WHEN ll.trn_debit_crdt_cd='C' THEN ll.trn_ldgr_entr_amt * -1 ELSE ll.trn_ldgr_entr_amt END) as True_Dollar_Value
-  
 from LD_LDGR_ENTR_T ll
 inner join CA_ACCOUNT_T ca
 on ll.ACCOUNT_NBR = ca.ACCOUNT_NBR
