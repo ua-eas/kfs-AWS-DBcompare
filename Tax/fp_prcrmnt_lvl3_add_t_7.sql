9b46b546e341ebf2c00bc0c62e290a239cb12078
Michael Hirst <mhirst@email.arizona.edu>
2017-12-12 14:02:06 -0700

    added year parameter for fp_prcrmnt

--- Tax/fp_prcrmnt_lvl3_add_t_7.sql
+++ Tax/fp_prcrmnt_lvl3_add_t_7.sql
@@ -9,8 +9,8 @@ SELECT CURRENT_TIMESTAMP FROM DUAL;
 spool ./data/fp_proc_7.xls
 
 select * from kulowner.FP_PRCRMNT_LVL3_ADD_T
-WHERE ORDER_DT  BETWEEN TO_DATE ('2017/08/01', 'yyyy/mm/dd')
-AND TO_DATE ('2017/08/07', 'yyyy/mm/dd')
+WHERE ORDER_DT  BETWEEN TO_DATE ('YEAR/08/01', 'yyyy/mm/dd')
+AND TO_DATE ('YEAR/08/07', 'yyyy/mm/dd')
 order by FDOC_NBR, OBJ_ID,VER_NBR,DEST_POSTAL  asc;
 
 
--- Tax/fp_prcrmnt_lvl3_add_t_gold.sql
+++ Tax/fp_prcrmnt_lvl3_add_t_gold.sql
@@ -8,8 +8,8 @@ SELECT CURRENT_TIMESTAMP FROM DUAL;
 spool ./data/fp_proc_gold.xls
 
 select * from kulowner.FP_PRCRMNT_LVL3_ADD_T
-WHERE ORDER_DT  BETWEEN TO_DATE ('2017/08/01', 'yyyy/mm/dd')
-AND TO_DATE ('2017/08/07', 'yyyy/mm/dd')
+WHERE ORDER_DT  BETWEEN TO_DATE ('YEAR/08/01', 'yyyy/mm/dd')
+AND TO_DATE ('YEAR/08/07', 'yyyy/mm/dd')
 order by FDOC_NBR, OBJ_ID,VER_NBR,DEST_POSTAL  asc;
 
 
