b8ed0d7c30fa05c9c05b6cf8dc9cfd24ec642ccb
Michael Hirst <mhirst@email.arizona.edu>
2017-12-12 12:20:30 -0700

    forgot to rename output file from TEST

--- LDGR/KFS3_LD_LDGR_ENTR_T.sql
+++ LDGR/KFS3_LD_LDGR_ENTR_T.sql
@@ -5,7 +5,7 @@ set headsep off
 
 SELECT CURRENT_TIMESTAMP FROM DUAL;
 
-spool ./data/TESTkfs3_ld_ldgr.xls
+spool ./data/kfs3_ld_ldgr.xls
 
 
 select ll.UNIV_FISCAL_YR, ll.ACCOUNT_NBR, ca.ACCOUNT_NM, cs.FUND_GRP_CD, cs.SUB_FUND_GRP_CD, ll.TRN_LDGR_ENTR_DESC, ll.POSITION_NBR, ll.EMPLID, ca.ORG_CD, co.ORG_NM, co.RPTS_TO_ORG_CD , SUM(CASE WHEN ll.trn_debit_crdt_cd='C' THEN ll.trn_ldgr_entr_amt * -1 ELSE ll.trn_ldgr_entr_amt END) as True_Dollar_Value  
