d5fc2f54ebd262c97253ba1acfc64d382922406c
Michael Hirst <mhirst@email.arizona.edu>
2017-12-14 13:27:47 -0700

    forgot spool off for tax_payee_t files

--- Tax/tax_payee_t_7.sql
+++ Tax/tax_payee_t_7.sql
@@ -9,3 +9,5 @@ spool ./data/tax_payee_t_7.xls
 
 select count(1) from KULOWNER.tax_payee_t;
 select * from KULOWNER.tax_payee_t order by payee_id , VNDR_HDR_GNRTD_ID asc;
+
+spool off
--- Tax/tax_payee_t_gold.sql
+++ Tax/tax_payee_t_gold.sql
@@ -10,3 +10,5 @@ spool ./data/tax_payee_t_gold.xls
 
 select count(1) from KULOWNER.tax_payee_t;
 select * from KULOWNER.tax_payee_t order by payee_id , VNDR_HDR_GNRTD_ID asc;
+
+spool off
