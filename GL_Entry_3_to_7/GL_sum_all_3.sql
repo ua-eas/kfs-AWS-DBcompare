04f99789ea6dc7d741c408e8ee13b2cbb5912ae2
Michael Hirst <mhirst@email.arizona.edu>
2017-12-19 13:25:37 -0700

    added new SQL to sum GL TRN_LDGR_ENTR_AMT

---
+++ GL_Entry_3_to_7/GL_sum_all_3.sql
@@ -0,0 +1,12 @@
+set colsep ','     
+set pagesize 0   
+set trimspool on 
+set headsep off     
+
+
+spool /home/mhirst/DB_Validation/query_results/sum_all3.csv
+
+select sum(TRN_LDGR_ENTR_AMT)  from  GL_ENTRY_T;
+
+spool off
+
