8e6f79a063f19c38f100d9e120a4e814fe5a9cc5
Michael Hirst <mhirst@email.arizona.edu>
2017-12-19 13:44:29 -0700

    fixed spool directory

--- GL_Entry_3_to_7/GL_sum_all_7.sql
+++ GL_Entry_3_to_7/GL_sum_all_7.sql
@@ -4,7 +4,7 @@ set trimspool on
 set headsep off     
 
 
-spool /home/mhirst/DB_Validation/query_results/sum_all7.csv
+spool ./data/sum_all7.csv
 
 select sum(TRN_LDGR_ENTR_AMT)  from  GL_ENTRY_T;
 
