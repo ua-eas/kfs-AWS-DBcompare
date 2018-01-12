d070d46a4a6aa1d2080752c95d4ede7298d056cf
Michael Hirst <mhirst@email.arizona.edu>
2018-01-12 13:40:41 -0700

    added 90 day date range for PO

---
+++ PUR/90_DAY_PUR_PO_T_3.sql
@@ -0,0 +1,15 @@
+set colsep ','     
+set pagesize 0   
+set trimspool on 
+set headsep off     
+
+SELECT CURRENT_TIMESTAMP FROM DUAL;
+
+
+spool ./data/PUR_PO_T_90_DAY_3.csv
+
+select * from PUR_PO_T where PO_CRTE_DT >= sysdate-90;
+
+
+spool off
+
