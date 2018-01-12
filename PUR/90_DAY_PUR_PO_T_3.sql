92bf2aeeba1d5d752e0a3b72a1c696902be9d37c
Michael Hirst <mhirst@email.arizona.edu>
2018-01-12 13:48:31 -0700

    added order by

--- PUR/90_DAY_PUR_PO_T_3.sql
+++ PUR/90_DAY_PUR_PO_T_3.sql
@@ -8,7 +8,7 @@ SELECT CURRENT_TIMESTAMP FROM DUAL;
 
 spool ./data/PUR_PO_T_90_DAY_3.csv
 
-select * from PUR_PO_T where PO_CRTE_DT >= sysdate-90;
+select * from PUR_PO_T where PO_CRTE_DT >= sysdate-90 order by 1 ASC;
 
 
 spool off
