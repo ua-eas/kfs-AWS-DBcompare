e72ce007241b546145718f92983d9a6b7ed37c4d
Michael Hirst <mhirst@email.arizona.edu>
2018-01-13 14:57:46 -0700

    added flowdown variable in shell script

--- jenkins_sql_jobs.sh
+++ jenkins_sql_jobs.sh
@@ -12,6 +12,7 @@ echo "KFS3_DB_HOST=$KFS3_DB_HOST" >> jenkins_params.properties
 echo "KFS3_DB_SERVICE=$KFS3_DB_SERVICE" >> jenkins_params.properties
 echo "KFS7_DB_HOST=$KFS7_DB_HOST" >> jenkins_params.properties
 echo "KFS7_DB_SERVICE=$KFS7_DB_SERVICE" >> jenkins_params.properties
+echo "FLOWDOWN=$FLOWDOWN" >> jenkins_params.properties
 
 cd $DIRECTORY
 
