9b6e4c8e96d3741cc8b80a5dfcfad1f91280f158
Michael Hirst <mhirst@email.arizona.edu>
2018-01-16 10:30:56 -0700

    .

--- test.sql
+++ test.sql
@@ -13,7 +13,7 @@ select * from FP_PRCRMNT_CARD_HLDR_DTL_T;
 spool off
 
 
-spool /home/mhirst/DB_Validation/FP_PRCRMNT_CARD_HLDR_DTL_T_7.csv
+spool FP_PRCRMNT_CARD_HLDR_DTL_T_7.csv
 
 select * from FP_PRCRMNT_CARD_HLDR_DTL_T;
 
