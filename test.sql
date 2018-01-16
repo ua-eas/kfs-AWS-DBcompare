7e530a106262745de95d65f9ee5f57194c8f4be0
Michael Hirst <mhirst@email.arizona.edu>
2018-01-16 10:30:32 -0700

    .

--- test.sql
+++ test.sql
@@ -6,7 +6,7 @@ set headsep off
 SELECT CURRENT_TIMESTAMP FROM DUAL;
 
 
-spool /home/mhirst/DB_Validation/FP_PRCRMNT_CARD_HLDR_DTL_T_3.csv
+spool FP_PRCRMNT_CARD_HLDR_DTL_T_3.csv
 
 select * from FP_PRCRMNT_CARD_HLDR_DTL_T;
 
