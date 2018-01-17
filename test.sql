8b252c1eafd2c77972d79d1e98b642a050b01dce
Michael Hirst <mhirst@email.arizona.edu>
2018-01-17 12:18:02 -0700

    testing gl blance t

--- test.sql
+++ test.sql
@@ -1,20 +1,21 @@
-set colsep ','     
+et colsep ','     
 set pagesize 0   
 set trimspool on 
 set headsep off     
+set numwidth 20
 
-SELECT CURRENT_TIMESTAMP FROM DUAL;
 
+spool ./data/gl_balance_t_3.csv
 
-spool ./data/3.csv
-
-select * from FP_PRCRMNT_CARD_HLDR_DTL_T;
+select * from  GL_BALANCE_T;
 
 spool off
 
 
-spool ./data/7.csv
 
-select * from FP_PRCRMNT_CARD_HLDR_DTL_T@DBLINK;
+
+spool ./data/gl_balance_t_7.csv
+
+select * from  GL_BALANCE_T@KFS7VAL
 
 spool off
