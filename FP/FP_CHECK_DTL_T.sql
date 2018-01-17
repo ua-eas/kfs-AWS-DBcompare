f39a0544881f71a8af10c4ac303fa9621328f990
Adam Kost <akost@eaglecrk.com>
2018-01-17 08:32:55 -0600

    add SQL for FP_CHECK_DTL_T

---
+++ FP/FP_CHECK_DTL_T.sql
@@ -0,0 +1,28 @@
+set colsep ','
+set pagesize 0
+set trimspool on
+set headsep off
+set numwidth 20
+
+SELECT CURRENT_TIMESTAMP FROM DUAL;
+
+
+spool ./data/ALL_FP_CHECK_DTL_T_3.csv
+SELECT * FROM FP_CHECK_DTL_T ORDER BY OBJ_ID;
+spool off
+
+spool ./data/ALL_FP_CHECK_DTL_T_7.csv
+SELECT * FROM FP_CHECK_DTL_T@DBLINK ORDER BY OBJ_ID;
+spool off
+
+
+
+spool ./data/ROW_COUNT_FP_CHECK_DTL_T_3.csv
+SELECT COUNT(*) FROM FP_CHECK_DTL_T;
+spool off
+
+spool ./data/ROW_COUNT_FP_CHECK_DTL_T_7.csv
+SELECT COUNT(*) FROM FP_CHECK_DTL_T@DBLINK;
+spool off
+
+
