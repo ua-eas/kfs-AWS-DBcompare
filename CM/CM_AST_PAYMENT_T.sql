2b24fd42d34b4703fed2715be14ffd067e2423e6
Michael Hirst <mhirst@email.arizona.edu>
2018-01-17 13:29:48 -0700

    added query for CM_AST_PAYMENT_T

---
+++ CM/CM_AST_PAYMENT_T.sql
@@ -0,0 +1,20 @@
+set colsep ','     
+set pagesize 0   
+set trimspool on 
+set headsep off     
+
+SELECT CURRENT_TIMESTAMP FROM DUAL;
+
+
+spool ./data/CM_AST_PAYMENT_T_3.csv
+
+select * from CM_AST_PAYMENT_T;
+
+spool off
+
+
+spool ./data/CM_AST_PAYMENT_T_7.csv
+
+select * from CM_AST_PAYMENT_T@DBLINK;
+
+spool off
