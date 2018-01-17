9051656606767ee7e75ed39fa959105b4a8ed6e1
Michael Hirst <mhirst@email.arizona.edu>
2018-01-17 11:59:42 -0700

    adding SQL for CHECK_RECONCILIATION_T

---
+++ CHK/CHECK_RECONCILIATION_T_SUM_CHECKNUM.sql
@@ -0,0 +1,21 @@
+et colsep ','     
+set pagesize 0   
+set trimspool on 
+set headsep off     
+set numwidth 20
+
+
+spool ./data/CHECK_RECONCILIATION_T_3.csv
+
+select SUM(CHECK_NBR) from CHECK_RECONCILIATION_T;
+
+spool off
+
+
+
+
+spool ./data/CHECK_RECONCILIATION_T_7.csv
+
+select SUM(CHECK_NBR) from CHECK_RECONCILIATION_T@DBLINK;
+
+spool off
