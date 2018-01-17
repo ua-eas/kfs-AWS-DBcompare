b44dc9708f9958ec4886e9050255aaeb3bc24b31
Michael Hirst <mhirst@email.arizona.edu>
2018-01-17 12:47:53 -0700

    added sql for sum account_nbr to gl balance t

---
+++ GL/gl_balance_t_SUM_ACCT_NBR.sql
@@ -0,0 +1,21 @@
+et colsep ','     
+set pagesize 0   
+set trimspool on 
+set headsep off     
+set numwidth 20
+
+
+spool ./data/gl_balance_t_3.csv
+
+select sum(ACCOUNT_NBR) from  GL_BALANCE_T
+
+spool off
+
+
+
+
+spool ./data/gl_balance_t_7.csv
+
+select sum(ACCOUNT_NBR) from  GL_BALANCE_T@DBLINK;
+
+spool off
