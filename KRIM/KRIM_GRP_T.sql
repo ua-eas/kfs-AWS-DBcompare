6752917553f11ab8377fed3cbaa8ee4e915ae964
Adam Kost <akost@eaglecrk.com>
2018-01-18 16:41:00 -0600

    add SQL script for KRIM_GRP_T

---
+++ KRIM/KRIM_GRP_T.sql
@@ -0,0 +1,17 @@
+set colsep ','
+set pagesize 0
+set trimspool on
+set headsep off
+set numwidth 20
+
+SELECT CURRENT_TIMESTAMP FROM DUAL;
+
+--KIM_TYP_ID is not included here because it is modified during the KFS 3 -> KFS 7 process.
+
+spool ./data/ALL_KRIM_GRP_T_3.csv
+SELECT GRP_ID, OBJ_ID, VER_NBR, GRP_NM, NMSPC_CD, GRP_DESC, ACTV_IND, LAST_UPDT_DT FROM KRIM_GRP_T ORDER BY OBJ_ID;
+spool off
+
+spool ./data/ALL_KRIM_GRP_T_7.csv
+SELECT GRP_ID, OBJ_ID, VER_NBR, GRP_NM, NMSPC_CD, GRP_DESC, ACTV_IND, LAST_UPDT_DT FROM rice.KRIM_GRP_T@KFS7GOLD ORDER BY OBJ_ID;
+spool off
