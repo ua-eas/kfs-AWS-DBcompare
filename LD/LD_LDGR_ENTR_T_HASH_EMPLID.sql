ca23b1e315831fa6a07a900baf4e047233879411
Michael Hirst <mhirst@email.arizona.edu>
2018-01-22 11:10:45 -0700

    forgot to change spool file location

--- LD/LD_LDGR_ENTR_T_HASH_EMPLID.sql
+++ LD/LD_LDGR_ENTR_T_HASH_EMPLID.sql
@@ -5,14 +5,14 @@ set headsep off
 set numwidth 20
 
 
-spool /home/mhirst/DB_Validation/query_results/3.csv
+spool ./data/LD_LDGR_ENTR_T_HASH_EMPID_3.csv
 
 SELECT EMPLID   FROM LD_LDGR_ENTR_T WHERE ORA_HASH(CONCAT(EMPLID, VER_NBR), 99, 5) = 0;
 
 spool off
 
 
-spool /home/mhirst/DB_Validation/query_results/7.csv
+spool ./data/LD_LDGR_ENTR_T_HASH_EMPID_7.csv
 
 SELECT EMPLID   FROM LD_LDGR_ENTR_T@DBLINK WHERE ORA_HASH(CONCAT(EMPLID, VER_NBR), 99, 5) = 0;
 
