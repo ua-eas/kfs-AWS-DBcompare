bd3f64899678dc3e9f88bfc9f280e66b2097a956
Michael Hirst <mhirst@email.arizona.edu>
2018-01-22 10:33:46 -0700

    forgot to change link to @DBLINK

--- LD/LD_LDGR_ENTR_T_HASH_EMPLID.sql
+++ LD/LD_LDGR_ENTR_T_HASH_EMPLID.sql
@@ -14,7 +14,7 @@ spool off
 
 spool /home/mhirst/DB_Validation/query_results/7.csv
 
-SELECT EMPLID   FROM LD_LDGR_ENTR_T@KFS7VAL WHERE ORA_HASH(CONCAT(EMPLID, VER_NBR), 99, 5) = 0;
+SELECT EMPLID   FROM LD_LDGR_ENTR_T@DBLINK WHERE ORA_HASH(CONCAT(EMPLID, VER_NBR), 99, 5) = 0;
 
 spool off
 
