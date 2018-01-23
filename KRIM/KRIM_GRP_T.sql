f749291bca66692ab3692a5b7d717c5ce88de7cb
Adam Kost <akost@eaglecrk.com>
2018-01-23 09:29:44 -0600

    Added exception for data added during the upgrade process.

--- KRIM/KRIM_GRP_T.sql
+++ KRIM/KRIM_GRP_T.sql
@@ -7,11 +7,11 @@ set numwidth 20
 SELECT CURRENT_TIMESTAMP FROM DUAL;
 
 --KIM_TYP_ID is not included here because it is modified during the KFS 3 -> KFS 7 process.
-
+--KFS 7 Upgrade adds a new group named "WorkflowAdmin". This is being excluded from the queries.
 spool ./data/ALL_KRIM_GRP_T_3.csv
-SELECT GRP_ID, OBJ_ID, VER_NBR, GRP_NM, NMSPC_CD, GRP_DESC, ACTV_IND, LAST_UPDT_DT FROM KRIM_GRP_T ORDER BY OBJ_ID;
+SELECT GRP_ID, OBJ_ID, VER_NBR, GRP_NM, NMSPC_CD, GRP_DESC, ACTV_IND, LAST_UPDT_DT FROM KRIM_GRP_T WHERE GRP_NM !='WorkflowAdmin' ORDER BY OBJ_ID;
 spool off
 
 spool ./data/ALL_KRIM_GRP_T_7.csv
-SELECT GRP_ID, OBJ_ID, VER_NBR, GRP_NM, NMSPC_CD, GRP_DESC, ACTV_IND, LAST_UPDT_DT FROM rice.KRIM_GRP_T@DBLINK ORDER BY OBJ_ID;
+SELECT GRP_ID, OBJ_ID, VER_NBR, GRP_NM, NMSPC_CD, GRP_DESC, ACTV_IND, LAST_UPDT_DT FROM rice.KRIM_GRP_T@DBLINK WHERE GRP_NM !='WorkflowAdmin' ORDER BY OBJ_ID;
 spool off
