eec655b33b8c586f391c6ffaa5ea62ce573e78aa
Michael Hirst <mhirst@email.arizona.edu>
2018-02-17 10:30:09 -0700

    fixed output filename to match SQL

--- CA/CA_ORG_EXT_T.sql
+++ CA/CA_ORG_EXT_T.sql
@@ -6,14 +6,14 @@ set headsep off
 SELECT CURRENT_TIMESTAMP FROM DUAL;
 
 
-spool ./data/CA_ACCOUNT_T_3.csv
+spool ./data/CA_ORG_EXT_T_3.csv
 
 select * from CA_ORG_EXT_T order by 1 ASC;
 
 spool off
 
 
-spool ./data/CA_ACCOUNT_T_7.csv
+spool ./data/CA_ORG_EXT_T_7.csv
 
 select  * from CA_ORG_EXT_T@DBLINK order by 1 ASC;
 
